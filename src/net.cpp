b036f8f039677df045669a85ad1db9bf987b56c4
SOELexicon <craig_wright156@hotmail.com>
2018-02-10 02:35:47 +0000

    half max outbound

--- src/net.cpp
+++ src/net.cpp
@@ -19,7 +19,7 @@
 using namespace std;
 using namespace boost;
 
-static const int MAX_OUTBOUND_CONNECTIONS = 16;
+static const int MAX_OUTBOUND_CONNECTIONS = 9;
 
 void ThreadMessageHandler2(void* parg);
 void ThreadSocketHandler2(void* parg);
