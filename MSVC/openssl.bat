a963b031a1823dd8093a0595b150688956c58043
Naruto TAKAHASHI <tnaruto@gmail.com>
2018-03-03 18:38:07 +0900

    deveploy lib and include to lib/openssl

--- MSVC/openssl.bat
+++ MSVC/openssl.bat
@@ -9,6 +9,12 @@ cd %CURDIR%
 git clone https://github.com/openssl/openssl.git
 cd openssl
 git checkout OpenSSL_1_0_2n
-perl Configure VC-WIN64A
-call ms\do_win64a
-nmake -f ms\nt.mak
+perl Configure VC-WIN64A || exit /b 1
+call ms\do_win64a || exit /b 1
+nmake -f ms\nt.mak || exit /b 1
+
+rmdir /S/Q %CURDIR%\lib\openssl
+mkdir %CURDIR%\lib\openssl\lib
+copy %CURDIR%\openssl\out32  %CURDIR%\lib\openssl\lib
+mkdir %CURDIR%\lib\openssl\include\openssl
+copy %CURDIR%\openssl\inc32\openssl  %CURDIR%\lib\openssl\include\openssl
