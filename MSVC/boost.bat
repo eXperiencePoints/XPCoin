8ecdba9b2950612741df302ea2bea46c1d7ee6c4
Naruto TAKAHASHI <tnaruto@gmail.com>
2018-03-09 00:09:03 +0900

    update and fix bat files.

--- MSVC/boost.bat
+++ MSVC/boost.bat
@@ -9,8 +9,8 @@ set CURDIR=%~dp0
 :: git checkout boost-1.66.0
 :: call bootstrap.bat || exit /b 1
 :: b2 toolset=msvc link=static,shared threading=multi variant=debug,release address-model=64 || exit /b 1
-del /S/Q boost_1_66_0-bi-msvc-all-32-64.7z
-rmkdir /S/Q boost_1_66_0
+del /S/Q boost_1_66_0-bin-msvc-all-32-64.7z
+rmdir /S/Q boost_1_66_0
 curl -OL https://dl.bintray.com/boostorg/release/1.66.0/binaries/boost_1_66_0-bin-msvc-all-32-64.7z
 7z x boost_1_66_0-bin-msvc-all-32-64.7z
 :: rmdir /S/Q %CURDIR%\lib\boost
